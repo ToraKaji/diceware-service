811657b2b9ae34e39e48011a267232d58eeb5e87
ToraKaji <ogradysjr@gmail.com>
2018-10-09 14:49:04 -0700

    Added more onto the service

--- src/main/java/edu/cnm/deepdive/dicewareservice/DicewareController.java
+++ src/main/java/edu/cnm/deepdive/dicewareservice/DicewareController.java
@@ -5,6 +5,7 @@ import edu.cnm.deepdive.cryptography.PassphraseGenerator;
 import edu.cnm.deepdive.cryptography.WordSource;
 import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,14 +15,20 @@ public class DicewareController {
 
   private ArtifactGenerator generator;
 
-  public DicewareController(WordSource source, Random rnd){
+  public DicewareController(WordSource source, Random rnd) {
     generator = new PassphraseGenerator(source, rnd);
 
   }
-@GetMapping("/diceware")
 
-  public String get(@RequestParam(name = "length", defaultValue = "1") int length){
-
+  @GetMapping(path = "/diceware", produces = "text/plain")
+  public String get(@RequestParam(name = "length", defaultValue = "1") int length) {
     return generator.gernerate(length);
   }
+
+  @GetMapping(path = "/diceware", produces = "application/json")
+  public String[] getJson(@RequestParam(name = "length", defaultValue = "1") int length) {
+  return get(length).split("\\s+");
+  }
+
+
 }
--- src/main/java/edu/cnm/deepdive/dicewareservice/DicewareServiceApplication.java
+++ src/main/java/edu/cnm/deepdive/dicewareservice/DicewareServiceApplication.java
@@ -1,7 +1,9 @@
 package edu.cnm.deepdive.dicewareservice;
 
+import javax.xml.ws.Response;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties.Async;
 
 @SpringBootApplication
 public class DicewareServiceApplication {
